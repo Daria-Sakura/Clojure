1cbfc331c4a2686935738e08c880d5d71cb1cdb2
Daria-Sakura <daria.zabrodets@gmail.com>
2014-11-20 15:09:13 +0300

    File structure updates.

---
+++ Clusterization/ClusterizationConsoleApp.clj
@@ -0,0 +1,21 @@
+(ns clusterization.core
+  (:gen-class))
+
+(defn clusterization [path destType]
+      (println path)
+      (println destType)
+      (read-file path)
+)
+
+(require '[clojure.java.io :as io])
+(defn read-file [filePath]
+
+)
+
+
+(defn -main
+  [& args]
+  (if (>= (count args) 2)
+
+    (println "Not enough arguments specified"))
+ )
