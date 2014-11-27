36414659ad89e6931fc27e8a4d0092a9b90a2f7b
Daria-Sakura <daria.zabrodets@gmail.com>
2014-11-27 22:15:26 +0300

    Clusterization

--- Clusterization/src/clusterization/core.clj
+++ Clusterization/src/clusterization/core.clj
@@ -1,14 +1,55 @@
 (ns clusterization.core
   (:gen-class)
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io])
+  (:require [clojure.math.numeric-tower :as math])
+  (import java.lang.Math))
 
 (def Ra 3)
 (def Rb (* 1.5 Ra))
 (def EpsHigh 0.5)
 (def EpsLow 0.15)
+(def alpha (/ 4 (math/expt Ra 2)))             
+(def beta  (/ 4 (math/expt Rb 2)))
+
+(defn calculate-potentials [points distance]
+  (let [point-potential 
+       (fn [x]
+         (try(let [len (reduce + 0 (map #(Math/exp (- (* alpha (distance % x)))) points))]
+         (list len x))(catch Exception e str ""))
+		)]
+  (map point-potential points))
+)
 
 (defn clusterization [points distance]
-      
+    (let [potentials (calculate-potentials points distance)]
+		(loop [potentials potentials clusters ()]
+			(let 	[	
+						P1  (or (first (first clusters)) 0)          
+						Pxk (apply max-key first potentials)       
+						Pk  (first Pxk)
+						Xk  (last Pxk)
+						potentials-n (map #(cons (- (first %)
+													(* Pk (Math/exp (- (* beta (distance (last %) Xk)))))
+												 ) 
+												 (rest %)
+											)
+										  potentials
+									  )
+					]
+				(cond
+				 (> Pk (* EpsHigh P1)) (recur potentials-n (conj clusters Pxk)) 
+				 (< Pk (* EpsLow P1)) clusters                              
+				 :else 
+				   (let [Dmin (apply min (map #(distance Xk (last %)) clusters))] 
+						(if (>= (+ (/ Dmin Ra) (/ Pk P1)) 1)
+							(recur potentials-n (conj clusters Pxk))                  
+							(recur (map #(if (= Xk (last %)) (list 0 Xk) %) potentials) clusters)
+						)
+					)
+				)
+			)
+		)
+	)
 )
 
 (defn split-string [string]
@@ -19,30 +60,28 @@
    )
  )
 
+ (def lines (atom []))
+ 
 (defn read-file [file-path]
-  (let [lines (atom [])]
-    (fn []
        (with-open [rdr (io/reader file-path)]
          (doseq [line (line-seq rdr)]
            (if (not= line "")
-             (swap! @lines conj (split-string line))
+             (swap! lines conj (split-string line))
            )
          )
        )
-    )
-  )
 )
 
-(defn hamming-distance [p1 p2]
+(defn hamming-distance [P1 p2]
   ( count
     (filter true? 
-            (map not= p1 p2)
+            (map not= P1 p2)
     )
   )
 )
 
-(defn euclid-distance [p1 p2]
-  (->> (map - p1 p2)
+(defn euclid-distance [P1 p2]
+  (->> (map - P1 p2)
        (map #(* % %))
        (reduce +) 
        (Math/sqrt)
@@ -54,7 +93,7 @@
   (if (>= (count args) 2) 
       (let [points (read-file (first args))
           distance (if (= (last args) "h") hamming-distance euclid-distance)]
-        (clusterization points distance)
+        (println (clusterization @lines distance))
       )    
       (println "Not enough arguments specified")
    )
